c6e67462f70a4491297ff42d6bb6c056d71e5b87
khand25 <drk46099@uga.edu>
2025-05-29 13:09:42 -0400

    added some code

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-// name of C# sharp project for namespace is ConsoleApp1 which is requred for any C# sharp code.
+// name of C# sharp project for namespace is ConsoleApp1 which is required for any C# sharp code.
 // similar to packages from Java
 namespace ConsoleApp1
 {
@@ -11,12 +11,29 @@ namespace ConsoleApp1
 
 
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Yo");
             string message = Console.ReadLine();
             Console.WriteLine($"You entered this message: {message}");
+            // the Write function is like a Java print statement where it prints
+            // content to the console BUT DOES NOT PASTE A NEW LINE CHARACTER AT THE
+            //END.
+            Console.Write("Stay on the same line");
+            Console.WriteLine("Test");
+
+            // This is a comment
+
+            /*
+            *
+            * This is
+            * a multline 
+            * comment
+            */
+
+            Console.WriteLine("\tDanyal");
+            //Console.Beep();
 
         }
     }
--- ConsoleApp1/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e300d750c06006276fab746905edc3137a9b97ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e32545b090a08b5e834e3fbaa809dd556095f5e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
