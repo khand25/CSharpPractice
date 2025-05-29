10e786bc1db6ab384b0d24adafcfa58511e60df2
khand25 <drk46099@uga.edu>
2025-05-29 13:15:30 -0400

    added some stuff

---
+++ NewProject/NewProject/VariablesIntro.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace NewProject
+{
+    class VariablesIntro
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
