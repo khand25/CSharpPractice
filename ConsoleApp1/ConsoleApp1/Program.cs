32381088a4a29f917b125a70e4a6b9145e457d7d
khand25 <drk46099@uga.edu>
2025-06-02 19:06:43 -0400

    updated the code

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Formats.Asn1;
 // name of C# sharp project for namespace is ConsoleApp1 which is required for any C# sharp code.
 // similar to packages from Java
 namespace ConsoleApp1
@@ -68,7 +69,7 @@ namespace ConsoleApp1
             long bigNumber = 90000000L;
             Console.WriteLine($"Age variable value: {age}, and bigNumber value: {bigNumber}");
 
-            //Decimal datatypes are double by defualt
+            //Decimal datatypes are double by defualt!
             double negative = -55.20;
             float precision = 5.000001F;
             Console.WriteLine($"negative variable value: {negative}, and precision value: {precision}");
@@ -82,15 +83,70 @@ namespace ConsoleApp1
 
             int x = 20, y = 19, z = 18;
 
-            /** Textual datatypes!! */
+            /** 
+            *Textual datatypes!! 
+            */
             // string datatype
             string name = "Danyal";
             // char datatype
             char letter = 'a';
             Console.WriteLine($"Your name is {name}");
-            // Converting Strings and numbers!
 
-        }
+            // Converting Strings to numbers!
+            string textAge = "-25";
+            // similar to Java's Integer.parseInt() function!
+            age = Convert.ToInt32(textAge);
+            Console.WriteLine(age);
+            // similar to Java's Long.parseLong() function!
+            // note don't add 'L' at the end of the string literal otherwise an exception will be thrown
+            bigNumber = Convert.ToInt64("9000000");
+            Console.WriteLine(bigNumber);
+            // similar to Java's Double.parseDouble() function!
+            negative = Convert.ToDouble("-55.5");
+            Console.WriteLine(negative);
+
+            /**
+            * Boolean dataype*/
+            bool value = true;
+            bool isMale = false;
+            Console.WriteLine(value);
+            Console.WriteLine(isMale);
+
+            /**
+            Operators
+            */
+
+            //int varaible
+            age = 23;
+            age++; //age = age + 1
+            age = age + 10; //age = age + 10
+            age += 10; // age = age + 10
+
+            age--; //age = age - 1
+            age = age - 10; //age = age - 10
+            age -= 10; // age = age - 10
+
+            age = 23;
+            age *= 10; // age = age * 10;
+            age = 23;
+            age /= 10; // age = age / 10 which is 2 as we are doing INTEER division and the decimal points truncates
+            Console.WriteLine($"The modified age is now {age}");
+            name = "Aba"; // which is a string vairalbe from above
+            name += " loves programming in C#!"; // name = name + (concatenation) " loves programming in C#!"
+            Console.WriteLine($"The name variable value is now: {name}");
+
+            /**
+            *Remainders in C# 
+            */
+            int firstNum = 10;
+            int secondNum = 3;
+            Console.WriteLine(firstNum / secondNum); // 3
+            Console.WriteLine(firstNum % secondNum); // 10 %3 is 1 as 10/3 has a remiander of 1.
+
+            /*** 
+            Var keyword
+            */
+        }   //Test function 
        
      } //class: Variables
 }
--- ConsoleApp1/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c7bc6c9d7a5499802186373aac3b37cb68ca2c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84a6ff7840c2f3c67f1ab43cac7a29e740e94b52")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
